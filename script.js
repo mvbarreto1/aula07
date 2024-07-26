16a1d8cb929100a91a03a8fa04247aae2dd29ce0
mvbarreto1 <marcusbarreto@outlook.com.br>
2024-07-25 23:33:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function numeroCubo (num){
+    let numero =  Number (prompt(digite um nuemro))
+    let cubo = numero*numero*numero
+    return (O numero ao cube é ${cubo})
+}
+
+console.log(numeroCubo());
